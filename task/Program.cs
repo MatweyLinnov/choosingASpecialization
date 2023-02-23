4df9f04602697d6ca4a96299994001bef0b37326
Matwey <linnov1703@gmail.com>
2023-02-23 14:17:39 +0300

    завершение описания алгоритма

--- task/Program.cs
+++ task/Program.cs
@@ -1,27 +1,34 @@
-﻿string[] firstArray = new string [5] {"123","11", "hi","Hello",":-:-:-"};
-string[] secondArray = new string[firstArray.Length];
+﻿int size = InputNum("Введите длину массива:\t");
 
-TransArray(firstArray, secondArray);
-PrintArray(secondArray, "Переведенный массив");
+string[] array = new string[size];
+string[] array1 = new string[array.Length];
 
-void TransArray(string[] firstArray, string[] secondArray)
+int count = 0;
+for (int i = 0; i < array.Length; i++)
 {
-    int count = 0;
-    for (int i = 0; i <firstArray.Length; i++)
+    System.Console.Write($"Введите элемент под индексом {i + 1}:\t");
+    array[i] = System.Console.ReadLine();
+    if (array[i].Length <= 3)
     {
-        if(firstArray[i].Length <=3)
-        {
-            secondArray[count] = firstArray[i];
-            count++;
-        }
+        array1[count] = array[i];
+        count++;
     }
 }
 
 void PrintArray(string[] array, string msg)
-{   
+{
     System.Console.WriteLine(msg);
-    for (int i = 0; i <array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        System.Console.WriteLine($"{array[i]}");
+        System.Console.WriteLine(array[i]);
     }
 }
+
+int InputNum(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+PrintArray(array, "Базовый массив");
+PrintArray(array1, "Измененный массив");
